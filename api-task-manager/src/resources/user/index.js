0a8604001381d0147fbc51d12dbc95f438025fa7
KuhArt <a.kukharenko@paralect.com>
2021-11-07 18:13:56 +0300

    [*] Remove unnecessary files

--- api-task-manager/src/resources/user/index.js
+++ api-task-manager/src/resources/user/index.js
@@ -4,8 +4,5 @@ require('./user.handler');
 
 const router = new Router();
 
-require('./get-current').register(router);
-require('./update-current').register(router);
-require('./list-users').register(router);
 
 module.exports = router.routes();
